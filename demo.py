fa42bc189f3f0f80abda0719c6b6203558291fdb
Zoheb007 <jahagirdarzoheb@gmail.com>
2024-08-12 15:03:43 +0530

    utils,exception added

--- demo.py
+++ demo.py
@@ -1,3 +1,10 @@
 from us_visa.logger import logger
+from us_visa.exception import USvisaException
+import sys
 
-logger.info("welcome to our custome log")
+#logger.info("welcome to our custome log")
+
+try:
+    a= 2/0
+except Exception as e:
+    raise USvisaException(e, sys)
