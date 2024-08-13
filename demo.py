30e1af02a81da59a572013446a0ead50fa04f944
Zoheb007 <jahagirdarzoheb@gmail.com>
2024-08-13 16:54:46 +0530

    mongo-client

--- demo.py
+++ demo.py
@@ -1,10 +1,3 @@
-from us_visa.logger import logger
-from us_visa.exception import USvisaException
-import sys
-
-#logger.info("welcome to our custome log")
-
-try:
-    a= 2/0
-except Exception as e:
-    raise USvisaException(e, sys)
+import os
+mongo_db_url = os.getenv('MONGO_DB_URL')
+print(mongo_db_url)
