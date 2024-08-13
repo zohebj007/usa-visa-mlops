1065ff18755ae67f62eb91c7d9a4c73c56cc5b10
Zoheb007 <jahagirdarzoheb@gmail.com>
2024-08-13 17:34:10 +0530

    chanegs

--- demo.py
+++ demo.py
@@ -1,3 +1,4 @@
-import os
-mongo_db_url = os.getenv('MONGO_DB_URL')
-print(mongo_db_url)
+from us_visa.pipeline.training_pipeline import TrainPipeline
+
+obj = TrainPipeline()
+obj.run_pipeline()
---
+++ us_visa/pipeline/training_pipeline.py
@@ -0,0 +1,54 @@
+import sys
+from us_visa.exception import USvisaException
+from us_visa.logger import logger
+from us_visa.components.data_ingestion import DataIngestion
+from us_visa.components.data_validation import DataValidation
+from us_visa.components.data_transformation import DataTransformation
+from us_visa.components.model_trainer import ModelTrainer
+
+
+from us_visa.entity.config_entity import (DataIngestionConfig,
+                                         DataValidationConfig,
+                                         DataTransformationConfig,
+                                         ModelTrainerConfig)
+
+from us_visa.entity.artifact_entity import (DataIngestionArtifact,
+                                            DataValidationArtifact,
+                                            DataTransformationArtifact,
+                                            ModelTrainerArtifact)
+
+
+class TrainPipeline:
+    def __init__(self):
+        self.data_ingestion_config = DataIngestionConfig()
+
+    
+    def start_data_ingestion(self) -> DataIngestionArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting data ingestion component
+        """
+        try:
+            logger.info("Entered the start_data_ingestion method of TrainPipeline class")
+            logger.info("Getting the data from mongodb")
+            data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
+            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+            logger.info("Got the train_set and test_set from mongodb")
+            logger.info(
+                "Exited the start_data_ingestion method of TrainPipeline class"
+            )
+            return data_ingestion_artifact
+        except Exception as e:
+            raise USvisaException(e, sys) from e
+        
+        
+
+    
+    def run_pipeline(self, ) -> None:
+        """
+        This method of TrainPipeline class is responsible for running complete pipeline
+        """
+        try:
+            data_ingestion_artifact = self.start_data_ingestion()
+            
+        except Exception as e:
+            raise USvisaException(e, sys)
